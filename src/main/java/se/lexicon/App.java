6e702b82d116265d5d2935aab51e15ec00f5cf36
Mia1251 <svanteomax@gmail.com>
2021-01-08 09:43:23 +0100

    Initial commit

---
+++ src/main/java/se/lexicon/App.java
@@ -0,0 +1,15 @@
+package se.lexicon;
+
+/**
+ * Hello Mehrdad!
+ *
+ */
+public class App {
+
+    public static void main( String[] args ) {
+
+        System.out.println( "Hello Mehrdad!" );
+        System.out.println( "Have a nice day!");
+        System.out.println( "First attempt without help!");
+    }
+}
